735792708f3a490435a813631a739e9ad78d9188
chaurasiyasimmi <chaurasiyasimmi@gmail.com>
2020-11-24 15:46:25 +0530

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,18 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        
+        private int myVar;
+        /*public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+        */
+        
+        
+    }
+}
